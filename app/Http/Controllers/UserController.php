03f4aafa9e751f03c098c1309b857e55fd7b7dc4
M. Aqilaz <muhaaqil6002@gmail.com>
2026-03-12 05:42:48 +0700

    prak_2.2_JS_04

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,8 +36,10 @@ class UserController extends Controller
 
         // $user = UserModel::find( 3);
         //$user = UserModel::firstWhere('level_id', 1);
-        $user = UserModel::findOr( 20, ['username', 'nama'], function (){ abort(404);
-        });
+        //$user = UserModel::findOr( 20, ['username', 'nama'], function (){ abort(404);});
+        //$user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+
         return view('user', ['data' => $user]);
     }
 }
