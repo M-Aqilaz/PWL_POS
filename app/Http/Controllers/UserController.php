5361d6466b0b47e263ccb58805f0b341b2ab3fc0
M. Aqilaz <muhaaqil6002@gmail.com>
2026-03-12 05:09:12 +0700

    prak_1_JS_04

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,13 +10,28 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
+        /*$data = [
             'username' => 'customer-1',
             'nama' => 'Pelanggan',
             'password' => Hash::make('12345'),
             'level_id' => 3
         ];
         UserModel::insert($data);
+        */
+
+        /*$data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+        */
+
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+        ];
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
