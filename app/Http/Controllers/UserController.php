935d72cbd24c1089ed814a385df8cc262a171cdd
M. Aqilaz <muhaaqil6002@gmail.com>
2026-03-12 05:32:27 +0700

    prak_2.1_JS_04

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,15 +25,19 @@ class UserController extends Controller
         UserModel::where('username', 'customer-1')->update($data);
         */
 
-        $data = [
+        /*$data = [
             'level_id' => 2,
             'username' => 'manager_tiga',
             'nama' => 'Manager 3',
             'password' => Hash::make('12345'),
         ];
         UserModel::create($data);
+        */
 
-        $user = UserModel::all();
+        // $user = UserModel::find( 3);
+        //$user = UserModel::firstWhere('level_id', 1);
+        $user = UserModel::findOr( 20, ['username', 'nama'], function (){ abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
