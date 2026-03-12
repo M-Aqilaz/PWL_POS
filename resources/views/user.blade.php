d70c350a827b94bb8a351735d33392dbd57edbe5
M. Aqilaz <muhaaqil6002@gmail.com>
2026-03-12 07:13:31 +0700

    prak_2.4_JS_04

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,7 +7,6 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         
-        {{-- 
         <tr>
             <th>ID</th>
             <th>Username</th>
@@ -20,14 +19,14 @@
             <td>{{ $data->nama }}</td>
             <td>{{ $data->level_id }}</td>
         </tr> 
-        --}}
 
-        <tr>
+
+        <!-- <tr>
             <th>Jumlah Pengguna</th>
         </tr>
         <tr>
             <td>{{ $data }}</td>
-        </tr>
+        </tr> -->
     </table>
 </body>
 </html>
